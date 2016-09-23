a3e2a2635bb348469c7b787083dc3b58b060f1af
antonio3424 <antonio3424@users.noreply.github.com>
2016-09-23 16:26:22 +0300

    Add files via upload
    Залил лабу

---
+++ laba1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double G, F, Y, x, a;
+    scanf("%lf", &x);
+    scanf("%lf", &x);
+    G = (7 * (54 * pow(a, 2) - 33 * a * x + 4 * pow(x, 2))) / (-4 * pow(a, 2) + a * x + 5 * pow(x, 2));
+    F = sin(3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
+    Y = asin(5 * pow(a, 2) - 9 * a * x + 4 * pow(x, 2));
+    printf("%.3f\n", G);
+    printf("%.3f\n", F);
+    printf("%.3f\n", Y);
+    return 0;
+}
