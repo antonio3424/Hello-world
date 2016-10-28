198d020f1d277fb2a8f42f3695d8db96d8afd65d
Vadik Kopylov <Kopylov.ddut@gmail.com>
2016-10-28 18:04:34 +0300

    Добавил2 лабу

--- laba2/main.c
+++ laba2/main.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main()
+{
+    char (r);
     double G, F, Y, x, a;
-    char r;
     scanf("%lf", &x);
-    scanf("%lf", &x);
-    printf("Ведите букву G,F,Y - ");
+    scanf("%lf", &a);
+    printf("Ведите букву G,F,Y");
     scanf("%s", &r);
-
+    printf("Введите переменную для Y от -1 до 1");
     switch (r) {
         case 'G': {
             G = (7 * (54 * pow(a, 2) - 33 * a * x + 4 * pow(x, 2))) / (-4 * pow(a, 2) + a * x + 5 * pow(x, 2));
             printf("G = %.3f\n", G);
             break;
         }
-        case 'F': {
-            F = sin(3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
+        case'F': {
+            F = (3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
             printf("F = %.3f\n", F);
             break;
-            case 'Y':
+        }
+        case 'Y':{
             Y = asin(5 * pow(a, 2) - 9 * a * x + 4 * pow(x, 2));
             printf("Y = %.3f\n", Y);
             break;
         }
 
-        default:break;}
-            return 0;
-
+        default:
+            break;
+    }
+    return 0;
 }
-
