6c8fd837e0279424bb063f60663c91cb321b35af
antonio3424 <antonio3424@users.noreply.github.com>
2017-01-06 16:44:38 +0400

    Add files via upload

---
+++ laba1/laba1py.py
@@ -0,0 +1,11 @@
+import math
+
+a = int(input("Введите a "))
+x = int(input("Введите x "))
+
+G = (7*(54*a**2-33*a*x+4*x**2))/(-4*a**2+a*x+5*x**2)
+print(G)
+F = 3**(-10*a**2+3*a*x+7*x**2)
+print(F)
+Y = math.asin(5*a**2-9*a*x+4*x**2)
+print(Y)
