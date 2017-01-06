b427fe75f18fa30f17986cbb68f980d73e207809
antonio3424 <antonio3424@users.noreply.github.com>
2017-01-07 07:49:23 +1100

    Update main.c

--- laba1/main.c
+++ laba1/main.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%lf", &x);
     scanf("%lf", &x);
     G = (7 * (54 * pow(a, 2) - 33 * a * x + 4 * pow(x, 2))) / (-4 * pow(a, 2) + a * x + 5 * pow(x, 2));
-    F = sin(3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
+    F = (3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
     Y = asin(5 * pow(a, 2) - 9 * a * x + 4 * pow(x, 2));
     printf("%.3f\n", G);
     printf("%.3f\n", F);
