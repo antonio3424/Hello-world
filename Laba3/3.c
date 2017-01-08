bebe9ddd0331c248615e047804af97f12b73ff11
antonio3424 <antonio3424@users.noreply.github.com>
2017-01-09 02:48:19 +0400

    Update 3.c

--- Laba3/3.c
+++ Laba3/3.c
@@ -3,8 +3,10 @@
 
 int main() {
     char (Function);
-    double x, x1, a, G, F, Y, step, pusk, completion;
+    double x, x1, a, G, F, Y, step;
     int k;
+    int razn;
+    ZANOVO:
 
     printf("введите нач. x:\n");
     scanf("%lf", &x);
@@ -22,37 +24,70 @@ int main() {
     scanf("%s", &Function);
 
 
-    printf("%s\n","начать restart - R, завершить finish - Q\n");
-    scanf("%i", &k);
-
     switch (Function) {
         case 'G':
-            for (x = pusk; x < completion; x += step);
+            for (x = 1; x < 10; x += step);
             {
                 if (-4 * pow(a, 2) + a * x + 5 * pow(x, 2) != 0) {
                     G = (7 * (54 * pow(a, 2) - 33 * a * x + 4 * pow(x, 2))) / (-4 * pow(a, 2) + a * x + 5 * pow(x, 2));
                     printf("G = %lf\n", G);
                 } else {
                     printf("не правильная функция\n");
+                break;
                 }
-            }
-            break;
+        }
+            printf("посчитайте другую функцию 1, завершить 0\n");
+            scanf("%d",&razn);
+            if (razn == 1)
+                goto ZANOVO;
+                break;
 
         case 'F':
-            for (x = pusk; x < completion; x += step);
-            F = (3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
-            printf("lf = F\n", F);
-            break;
+            for (x = 1; x < 10; x += step);
+            {
+                if ("x > 0, x1 < 15\n") {
+                    F = (3 * (-10 * pow(a, 2) + 3 * a * x + 7 * pow(x, 2)));
+                    printf("F = %lf\n", F);
+                }
+                else
+                {
+                    printf("не правильная функция\n");
+                break;
+                }
+            }
+            printf("посчитайте другую функцию 1, завершить 0\n");
+            scanf("%d",&razn);
+            if (razn == 1)
+                goto ZANOVO;
+                break;
 
         case 'Y':
-            for (x = pusk; x < completion; x += step);
-            Y = asin(5 * pow(a, 2) - 9 * a * x + 4 * pow(x, 2));
-            printf("lf = Y\n", Y);
-            break;
+            for (x = -1; x < 1; x += step);
+            {
+                if ("x > 1, x1 < 10\n")
+                {
+                    Y = asin(5 * pow(a, 2) - 9 * a * x + 4 * pow(x, 2));
+                    printf("Y = %lf\n", Y);
+                }
+                else
+                {
+                    printf("не правильная функция\n");
+                break;
+                }
+
+            }
+            printf("посчитайте другую функцию 1, завершить 0\n");
+            scanf("%d",&razn);
+            if (razn == 1)
+                goto ZANOVO;
+                break;
+
+
         default:
-            printf("начать restart - R, завершить finish - Q\n");
+            printf("для выполнения нажмите G, F, Y\n");
             scanf("%i", &k);
             break;
     }
+
     return 0;
 }
